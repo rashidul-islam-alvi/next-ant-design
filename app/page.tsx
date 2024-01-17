caedf5ed1a7740f2886329bddfb121ed9375ed9b
Rashidul Islam Alvi <rashidul537@gmail.com>
2024-01-17 14:43:48 +0600

    getting started

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <main className="p-3">
+      <Button type="default">Button</Button>
+    </main>
+  );
+}
---
+++ components/sign-in-form.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignInForm = () => {
+  return <div>sign-up</div>;
+};
+
+export default SignInForm;
